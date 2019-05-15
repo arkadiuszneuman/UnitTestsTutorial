216dc33641d7e8f6782b485fa9dc8c4bbca6ef6d
Arkadiusz Neuman <arkadiusz.neuman@ialbatros.com>
2019-05-15 15:55:31 +0200

    Finished

--- UnitTestsTutorial.Tests/WithDependencies/CurrencyConverterTests.cs
+++ UnitTestsTutorial.Tests/WithDependencies/CurrencyConverterTests.cs
@@ -53,17 +53,33 @@ namespace UnitTestsTutorial.Tests.WithDependencies
 
             var currencyToConvert = "EUR";
 
-            var sut = new Fixture()
+            var fixture = new Fixture();
+            var sut = fixture
+                .WithExchangeRate(15)
                 .Configure();
 
             // act
             sut.ConvertPrice(price, currencyToConvert);
 
             // assert
+            fixture.ExchangeRatesInCache.Should().BeEquivalentTo(new Fixture.ExchangeRateInCache
+            {
+                CurrencyFrom = "GBP",
+                CurrencyTo = "EUR",
+                ExchangeRate = 15
+            });
         }
 
         private class Fixture
         {
+            public class ExchangeRateInCache
+            {
+                public string CurrencyFrom { get; set; }
+                public string CurrencyTo { get; set; }
+                public decimal ExchangeRate { get; set; }
+            }
+
+            public List<ExchangeRateInCache> ExchangeRatesInCache { get; set; } = new List<ExchangeRateInCache>();
             private int _exchangeRate;
 
             public Fixture WithExchangeRate(int exchangeRate)
@@ -79,6 +95,16 @@ namespace UnitTestsTutorial.Tests.WithDependencies
                     mock.Mock<ICurrencyAccess>()
                         .Setup(x => x.GetExchangeRate(It.IsAny<string>(), It.IsAny<string>()))
                         .Returns(_exchangeRate);
+
+                    mock.Mock<ICurrencyAccess>()
+                        .Setup(x => x.AddToCache(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<decimal>()))
+                        .Callback<string, string, decimal>((currencyFrom, currencyTo, exchangeRate) =>
+                            ExchangeRatesInCache.Add(new ExchangeRateInCache
+                            {
+                                CurrencyFrom = currencyFrom,
+                                CurrencyTo = currencyTo,
+                                ExchangeRate = exchangeRate
+                            }));
 
                     return mock.Create<CurrencyConverter>();
                 }
